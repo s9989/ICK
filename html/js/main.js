5de7e0bd5dab5b1050c89a0c571f563359aab77a
Jakub Kurylowicz <czterolistny@gmail.com>
2020-01-17 20:40:43 +0100

    part4

--- html/js/main.js
+++ html/js/main.js
@@ -1,11 +1,18 @@
 $(document).ready(function () {
+
     $('.menu_button').on('click', function() {
         $(this).find('i').toggleClass('active');
         $('.mask').toggle();
         $('.menu').toggle();
     });
+
     $('.size img').on('click', function () {
         $('.size img').removeClass('selected');
         $(this).addClass('selected');
-    })
+    });
+
+    $('.favourites .favourite').on('click', function () {
+        $(this).toggleClass('selected');
+    });
+
 });
